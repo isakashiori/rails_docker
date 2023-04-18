cafb63103d1c4e57caaed65db8ba78928b49c4d5
isakashiori <isslamdunk0000@gmail.com>
2023-04-18 06:56:20 +0000

    add: カレントユーザーの設定とログインログアウトの表示設定

---
+++ app/controllers/staff/base.rb
@@ -0,0 +1,19 @@
+class Staff::Base < ApplicationController
+  private def current_staff_member
+    if session[:staff_member_id]
+      @current_staff_member ||=
+        StaffMember.find_by(id: session[staff_member_id])
+    end
+  end
+
+  helper_method :current_staff_member
+end
+
+# session[:staff_member_id]にnilがfalse以外の値が入っているのなら処理が行われる
+# staffmemberのモデルの中から今ログインしているスタッフのidを探してきてインスタンス変数の中に格納する
+# これでどこでも今ログインしているスタッフの情報を使うことができる
+# ||=を使って遅延初期化というテクニックを使っている
+# これは初回時にcurrent_staff_memberメソッドを呼んだ時の@current_staff_memberには何も
+# 入っていないから右辺の処理が走るが
+# 2回目以降は@current_staff_memberには値がもう格納されているので右辺の処理は行われず
+# データペースへのアクセス回数を減らすことができてパフォーマンスの向上が狙えるらしい
