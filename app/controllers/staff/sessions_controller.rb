2c82950112d8b9795837e99915075733deb6babe
isakashiori <isslamdunk0000@gmail.com>
2023-04-20 00:25:59 +0000

    add: 演習問題まで完了

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -14,14 +14,24 @@ class Staff::SessionsController < Staff::Base
     if @form.email.present?
       staff_member =
         StaffMember.find_by("LOWER(email) = ?", @form.email.downcase)
+        # staffmemberモデルの中からfind_byでマッチしたレコードを取得してstaff_member変数の中に格納している
+      # find_byはカラム名もしくは検索条件によってデータを探しに行く
+      # 上記は検索条件によってデータを探しにいっている
+      # またfind_byは最初にマッチしたデータしか取得してこない
+      # それでもいい理由に関しては、メアドはユニークで設定しているから重複がないから
+      # @formで取得してきたレコードのメアドをプレスホルダーに格納して検索していく
+      # それをstaf_member変数の中に格納していく
     end
     if Staff::Authenticator.new(staff_member).authenticate(@form.password)
-      
+      # Staff::Authenticatorモデルのインスタンスを作成して引数にstaff_memberを入れる
+      # staff_memberの変数とは一個上のif文の中で定義されている
+      # authenticateメソッドはサービスオブジェクトに定義されているメソッドになっている
+
       if staff_member.suspended?
         flash.now.alert = "アカウントが停止されています"
         render action: "new"
       else
-        session[:staff_member_id] = staff_member_id
+        session[:staff_member_id] = staff_member.id
         flash.notice = "ログインしました"
         redirect_to :staff_root
       end
